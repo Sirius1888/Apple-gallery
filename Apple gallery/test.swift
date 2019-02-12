26d63f7b706e6b030ffff67dd54255db4e940a79
sirius <sirius@macbook-pro-sirius.local>
2019-02-12 14:29:07 +0600

    second commit

---
+++ Apple gallery/test.swift
@@ -0,0 +1,13 @@
+//
+//  test.swift
+//  Apple gallery
+//
+//  Created by sirius on 12.02.2019.
+//  Copyright © 2019 sirius. All rights reserved.
+//
+
+import UIKit
+
+class test: NSObject {
+
+}
